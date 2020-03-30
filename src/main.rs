1143dfb87054a33d15c0b246b89ba1f4129b55ec
Richard Mitic <richard.h.mitic@gmail.com>
2020-03-30 17:45:29 +0200

    Rename VelvetImpluseLocations -> OVNImpulseLocations

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 extern crate velvet_noise;
-use velvet_noise::VelvetImpulseLocations;
+use velvet_noise::OVNImpulseLocations;
 
 pub fn main() {
-    let mut vil = VelvetImpulseLocations::new(4410, 44100);
+    let mut vil = OVNImpulseLocations::new(4410, 44100);
     for i in 0..100 {
         println!("{} {}", i, vil.next().unwrap());
     }
