84df28a108bab9bad7732cc1e220c61b9ea0387c
Richard Mitic <richard.h.mitic@gmail.com>
2020-04-03 23:17:28 +0200

    Read file using Signal trait

--- src/main.rs
+++ src/main.rs
@@ -2,36 +2,44 @@ extern crate hound;
 extern crate sample;
 extern crate velvet_noise;
 
+use std::{io, fs};
+use sample::{signal, Sample, Signal, I24};
+use hound::WavReader;
+
 fn convolve_kern(samples: &[f32], kern: &[(usize, f32)]) -> f32 {
     kern.iter().map(|(i, x)| samples[*i] * x).sum::<f32>()
 }
 
 /// Create an endless sound as decribed in http://dafx.de/paper-archive/2018/papers/DAFx2018_paper_11.pdf
-pub fn main() {
-    let mut reader = hound::WavReader::open("guitar_chord_mono_2.wav").unwrap();
-    let samples = reader
-        .samples::<i32>()
-        .map(|s| sample::conv::i24::to_f32(sample::types::i24::I24::new_unchecked(s.unwrap())))
-        .collect::<Vec<f32>>();
+fn process(reader: WavReader<io::BufReader<fs::File>>) {
 
-    // This is used for subsequent convolution coefficients
-    let mut choice = velvet_noise::Choice::classic();
+    // read samples from file
+    // TODO: make this generic over channels and sample type
+    let spec = reader.spec().clone();
+    let duration = reader.duration();
+    let sample_iter = reader.into_samples().filter_map(Result::ok).map(|x| I24::new_unchecked(x).to_sample::<f32>());
+    let sample_signal = signal::from_interleaved_samples_iter::<_, [f32; 1]>(sample_iter);
+    let samples = sample_signal.until_exhausted().map(|f| f[0]).collect::<Vec<f32>>();
 
     // Create 10 seconds of audio
     let n_seconds = 10;
-    let n_samples = reader.spec().sample_rate * n_seconds;
-    let sample_rate = reader.spec().sample_rate as f32;
-    let duration_s = reader.duration() as f32 / sample_rate;
+    let n_samples = spec.sample_rate * n_seconds;
+    let sample_rate = spec.sample_rate as f32;
+    let duration_s = duration as f32 / sample_rate;
 
     // paper suggests 32 simultaneous taps
     let density = 32. / duration_s;
 
-    let mut initial_taps = velvet_noise::VelvetNoiseKernel(
+    // initialise an array of delay taps
+    let mut taps = velvet_noise::VelvetNoiseKernel(
         velvet_noise::OVNImpulseLocations::new(density as usize, sample_rate as usize),
         velvet_noise::Choice::classic(),
     )
     .take_while(|(i, _)| i < &samples.len())
     .collect::<Vec<(usize, f32)>>();
+
+    // used for subsequent convolution coefficients since the previous kernel is already taken
+    let mut choice = velvet_noise::Choice::classic();
 
     // output
     let spec = hound::WavSpec {
@@ -43,25 +51,30 @@ pub fn main() {
     let mut writer = hound::WavWriter::create("out.wav", spec).unwrap();
 
     let max_index = samples.len() - 1;
-    let gain = 0.3;
+    let gain = 0.1;
 
     for _ in 0..n_samples {
         writer
-            .write_sample(convolve_kern(&samples, &initial_taps) * gain)
+            .write_sample(convolve_kern(&samples, &taps) * gain)
             .unwrap();
 
-        for tap in initial_taps.iter_mut() {
+        for tap in taps.iter_mut() {
             *tap = (tap.0 + 1, tap.1);
         }
 
-        // move taps along delay lines
-        let length_before = initial_taps.len();
-        initial_taps.retain(|&tap| tap.0 <= max_index);
-        let length_after = initial_taps.len();
+        // move taps along delay line
+        let length_before = taps.len();
+        taps.retain(|&tap| tap.0 <= max_index);
+        let length_after = taps.len();
         for _ in 0..(length_before - length_after) {
-            initial_taps.push((0, choice.next().unwrap()));
+            taps.push((0, choice.next().unwrap()));
         }
     }
 
     writer.finalize().unwrap();
+}
+
+pub fn main() {
+    let reader = WavReader::open("guitar_chord_mono.wav").unwrap();
+    process(reader);
 }
